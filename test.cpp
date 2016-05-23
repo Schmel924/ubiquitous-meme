f38025cadbe9eda617f0bf8f1ef98f3d7a8aea12
Schmel924 <Schmel924@gmail.com>
2016-05-23 17:29:55 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,3 +8,14 @@ std::cout <<"Is this shit working?"<< std::endl;
 getch();
 return 0;
 }
+
+
+git clone https://github.com/nanorc/nanorc.git
+cd nanorc
+make install
+Add these lines to the ~/.nanorc
+
+include ~/.nano/syntax/html.nanorc
+include ~/.nano/syntax/css.nanorc
+include ~/.nano/syntax/php.nanorc
+include ~/.nano/syntax/ALL.nanorc
