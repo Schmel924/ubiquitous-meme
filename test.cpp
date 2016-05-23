c946504d9fd615fe0ed9efa26450d503f14331a6
Schmel924 <Schmel924@gmail.com>
2016-05-23 23:47:19 +0300

    BJeppa has come
    D
    A
    A
    C

--- test.cpp
+++ test.cpp
@@ -10,12 +10,4 @@ return 0;
 }
 
 
-git clone https://github.com/nanorc/nanorc.git
-cd nanorc
-make install
-Add these lines to the ~/.nanorc
 
-include ~/.nano/syntax/html.nanorc
-include ~/.nano/syntax/css.nanorc
-include ~/.nano/syntax/php.nanorc
-include ~/.nano/syntax/ALL.nanorc
